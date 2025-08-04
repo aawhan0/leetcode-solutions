be0af6afd8cb088ce0d18fd2b941f2bf66c10949
Aawhan <114390114+aawhan0@users.noreply.github.com>
2025-08-04 10:03:15 +0530

    Time: 11 ms (36.12%) | Memory: 17.7 MB (88.81%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        # int_max = 2**31 -1
+        # int_min = -2**31
+        # no need because overflow aint happening here.
+
+        if x<0:
+            return False
+        original = x
+        rev = 0
+
+        while x!= 0:
+            ld = x% 10
+            rev = rev*10 + ld
+            x//= 10
+        
+        if original == rev:
+            return True
+        else:
+            return False
