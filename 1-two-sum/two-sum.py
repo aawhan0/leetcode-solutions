7e24ade57f22f0fa9197d3232a65435f30e67427
Aawhan <114390114+aawhan0@users.noreply.github.com>
2025-11-13 18:00:43 +0530

    Time: 0 ms (100.00%) | Memory: 19 MB (24.02%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,10 +1,9 @@
-from typing import List
-
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        num_map = {}  # value : index
-        for i, num in enumerate(nums):
-            diff = target - num
-            if diff in num_map:
-                return [num_map[diff], i]
-            num_map[num] = i
+        hashMap = {} #creating a dictionary (key-value pair)
+        for i,num in enumerate(nums):
+            if target - num in hashMap:
+                return [hashMap[target - num], i] 
+            hashMap[num] = i
+
+        return []
